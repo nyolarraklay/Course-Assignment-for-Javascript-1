b7eec50ef85c22b40037188493120a4ddf69e52e
nyolarraklay <nyolosorii@gmail.com>
2023-02-22 12:29:10 +0100

    edited

--- Ernesto_Jr-Osorio-js1-ca/detailsRecipe.js
+++ Ernesto_Jr-Osorio-js1-ca/detailsRecipe.js
@@ -1,54 +1,79 @@
 const container = document.querySelector(".details");
-const title = document.querySelector("title")
+const title = document.querySelector("title");
+const breadcrumb = document.querySelector(".breadcrumbs");
 
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
-
-console.log(params)
-
 const id = params.get("id");
-
-console.log(id)
 
 const key = "daf0594025214320ab5b190b445c944e";
 const url = `https://api.spoonacular.com/food/menuItems/${id}?&apiKey=${key}`;
 
-async function getFood (){
-  
-  try {
-    const response = await fetch (url);
-    const chicken = await response.json();
-   
-    console.log(chicken)
-
-    container.innerHTML = "";
-
-    createHtml(chicken);
-    
+  async function getFood (){
+    try {
+      const response = await fetch (url);
+      return await response.json();
+    }
+      catch(error) {
+        console.log(error);
+        container.innerHTML = message("error", error);
+   }
+      
   }
-  catch(error) {
-    console.log(error);
-    container.innerHTML = message("error", error);
- }
-
-}
-getFood ();
  
+
   
-  function createHtml(chicken) {
+
+function createHtml (chicken) {
+  
   title.innerHTML = `${chicken.title}`;
-  container.innerHTML += `<h1>${chicken.title}</h1> 
-  <div class="content-container">
-  <p> <span>Restaurant:</span> ${chicken.restaurantChain} </p>
-  <img src="${chicken.image}" alt="delicious burger from ${chicken.restaurantChain}"/></div>
-  <div class="description">
-  <h3>Nutrition</h3><li>Calories: ${chicken.nutrition.calories}</li>
-  <li>Carbs: ${chicken.nutrition.carbs}</li>
-  <li>Fats:${chicken.nutrition.fat}</li>
-  <li>Protein:${chicken.nutrition.protein}</li>
-  </div>
-  </div>`
-  }
+  const a = document.createElement("a");
+  a.href = "index.html";
+  a.innerText = `back to chicken menu`;
+  const chickenName = document.createElement("h1");
+  chickenName.innerText = chicken.title;
+  const divContent = document.createElement("div");
+  divContent.classList.add("content-container");
+  const restaurant = document.createElement("p");
+  const span = document.createElement("span");
+  span.innerText = `Restaurant:`;
+  restaurant.innerText = " " + chicken.restaurantChain;
+  const imageDetails = document.querySelector("div");
+  imageDetails.innerHTML = `<img>`;
+  imageDetails.querySelector("img").src = chicken.image;
+
+  const divNutrients = document.createElement("div");
+  divNutrients.classList.add("description");
+  const nutrition = document.createElement("h3");
+  nutrition.innerText = "Nutrition";
+
+  const ol = document.createElement("ul");
+  const li1 =document.createElement("li");
+  li1.innerText = `Calories: ${chicken.nutrition.calories}`;
+  const li2 =document.createElement("li");
+  li2.innerText = `Carbs: ${chicken.nutrition.carbs}`;
+  const li3 =document.createElement("li");
+  li3.innerText =  `Fats: ${chicken.nutrition.fat}`;
+  const li4 =document.createElement("li");
+  li4.innerText =  `Protein: ${chicken.nutrition.protein}`;
+  ol.append(li1, li2, li3, li4);
+
+
+  divContent.append(a, chickenName, restaurant, imageDetails);
+  restaurant.prepend(span )
+  divNutrients.append(nutrition, ol)
+
+  document.body.prepend(divContent, divNutrients);
+  
+}
+
+async function postsPage(){
+  const posts = await getFood(url);
+  createHtml(posts);
+  return "All data has been fetched and rendered!";
+}
+
+postsPage().then(console.log)
 
 
 
--- Ernesto_Jr-Osorio-js1-ca/script.js
+++ Ernesto_Jr-Osorio-js1-ca/script.js
@@ -34,5 +34,3 @@ async function getFood (){
 
 getFood ();
 
-
-
